c810053def70c98b82ff15f30eb6bc2601bf0ccf
Alan Joseph <alanjoseph52006@gmail.com>
2026-08-18 18:27:13 +0530

    Add weather check and Twilio SMS notification
    Refactor code to integrate weather API and Twilio messaging.

--- main.py
+++ main.py
@@ -1,38 +1,51 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
+import requests
+from twilio.rest import Client
 
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
 import os
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+api_key = os.environ.get("3fbaf3ad51cc9977663dfe91535f7698")
+account_sid = os.environ.get("ACb9138774c957331f900dd09ee60407f6")
+auth_token = os.environ.get("450e4b0f6b7c3ffd7c4773d44e90aefb")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+MY_LAT = 11.981659
+MY_LONG = 75.382952
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+
+parameters={
+"lat":11.981685,
+"lon":75.383226,
+"cnt": 4,
+"appid":api,
+"formatted":0
+}
+
+
+# Initialize the Twilio client
+client = Client(account_sid, auth_token)
+
+api_endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+data = requests.get(api_endpoint,params=parameters)
+weather_data = data.json()
+print(weather_data)
+weather_id_lst = []
+will_rain = False
+
+for hour in weather_data["list"]:
+
+
+    weather_id= int(hour["weather"][0]["id"])
+    if weather_id < 700:
+        will_rain = True
+if will_rain:
+    print("Bring Umberlla")
+    # Send the message
+    message = client.messages.create(
+        body="Purath pokumbo kuda☂️ edukk kunne..!Mazha peyyum⛈️!",
+        from_="+12544574675",  # Your Twilio phone number
+        to="+919946890737"  # The recipient's phone number (with country code)
+    )
+    # Print the unique message SID to confirm it was sent
+    print(f'Message sent successfully! Message SID: {message.sid}')
+
+
